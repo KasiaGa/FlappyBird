ff68155558b9f707ef9c8a0fd028e3715a0068a7
KasiaGa <kasik2121@gmail.com>
2015-07-31 17:19:42 +0200

    improved menu and added actions to buttons

--- Flappy/app/src/main/java/pl/kasmagdam/flappy/MainActivity.java
+++ Flappy/app/src/main/java/pl/kasmagdam/flappy/MainActivity.java
@@ -1,9 +1,12 @@
 package pl.kasmagdam.flappy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button newGameButton = (Button) findViewById(R.id.new_game_button);
+        final Intent intent = new Intent(this, GameActivity.class);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+        final Button instructionsButton = (Button) findViewById(R.id.instructions_button);
+        final Intent intent2 = new Intent(this, InstructionsActivity.class);
+        instructionsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent2);
+            }
+        });
+
+        final Button scoresButton = (Button) findViewById(R.id.scores_button);
+        final Intent intent3 = new Intent(this, ScoresActivity.class);
+        scoresButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent3);
+            }
+        });
     }
 
     @Override
@@ -35,4 +61,10 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*public void startGame(Intent intent) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }*/
 }
